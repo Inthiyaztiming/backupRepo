061dc31c86ba95547e9017f0f9e541cb38d751a3
Inthiyaztiming <inthiyaz.m@timingindia.com>
2025-08-19 15:16:20 +0530

    Update Notes3.java

--- Notes3.java
+++ Notes3.java
@@ -45,3 +45,64 @@
 //4.I can switch to FirefoxDriver or EdgeDriver without changing the rest of the code, because all drivers implement WebDriver
 //5.driver is the same reference variable (of type WebDriver)
 //6.But it points to different objects at runtime- This is runtime polymorphism
+
+
+//------------------------Null Pointer Exception--------------------------------------
+
+//When we declare an instance or class variable (like an array or object) but don’t initialize it, it defaults to null in Java. 
+//After declaring If we then try to use it, Java throws a Null Pointer Exception because there’s no actual object behind that reference.(Runtime)
+
+//To avoid this, always initialize such variables before use, for example:
+//int[] numbers = new int[5];
+
+//----------------------If asked how to prevent or handle NullPointerException, you can mention----------------
+//Use default initialization.
+//Add null checks before use.
+//Use Optional in modern Java (Java 8+).
+//Avoid returning null from methods when possible.
+
+
+//----------------------------Frames--------------------------------------
+//driver.switchTo().parentFrame() moves one level up in the frame hierarchy.
+//If we are in nested frame and on frame 3, calling parentframe method once goes to frame 2, twice goes to frame 1, and so on.
+//driver.switchTo().defaultContent() immediately switches back to the main page (top-level document), exiting all frames regardless of nesting depth.
+
+//Use parentFrame() to move up step-by-step, and defaultContent() to jump out of all frames at once.
+
+//-----------------------Diamond Problem-------------------------
+//A
+/// \
+//B   C
+//\ /
+//D
+//
+//The Diamond Problem occurs in multiple inheritance when a class inherits the same method from two different paths, forming a 
+//diamond shape in the inheritance graph.
+
+//For example:
+//Class B inherits from A
+//Class C also inherits from A
+//Class D inherits from both B and C
+//
+//If class A has a method (e.g., show()), and both B and C override it, now when D calls show(), which version should the JVM call — B’s or C’s?
+//This creates ambiguity, which is the diamond problem.
+
+//How Java Handles It:
+//To avoid this problem, Java does not support multiple inheritance of classes.
+//Java does support multiple inheritance of interfaces, because:
+//Interfaces traditionally had no implementation, only method declarations.
+//So, there’s no conflict, since the implementing class provides the method.
+
+//But after Java 8…
+//Java introduced default methods in interfaces (methods with implementation).
+//This can cause the Diamond Problem again, if two interfaces provide the same default method.
+
+//To solve this:-
+//Java forces the class to override the method to resolve ambiguity and explicitly resolve the 
+//conflict using InterfaceName.super.method()
+
+
+
+
+
+
