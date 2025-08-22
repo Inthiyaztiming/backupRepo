ca74851032efd9960f7df48ea40d8c2ad7b41880
Inthiyaztiming <inthiyaz.m@timingindia.com>
2025-08-22 17:43:02 +0530

    Update Notes8.java

--- Notes8.java
+++ Notes8.java
@@ -42,3 +42,32 @@
 //WebElement newElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("aaa")));//Then find the new instance of the element
 
 
+//ElementNotInteractableException in Selenium occurs when:
+
+//The element is present in the DOM but not interactable on the UI at that moment.
+
+//Common Causes:
+
+//The element is hidden (e.g., display: none, or not yet rendered) or The element is disabled 
+//or The element becomes available only after a specific user action
+
+//Suppose there is a form with two radio buttons — Male and Female.
+
+//When the user selects Female, an additional section appears to upload supporting documents. This section stays hidden when Male is selected.
+//During automation, if I directly tried to interact with the document upload element after selecting Male, Selenium threw:
+//ElementNotInteractableException ,Because the element is:
+//Present in the DOM
+//But not visible or enabled for interaction
+
+//How to Handle:
+
+//To avoid this exception, we can check the element’s state using the following WebElement methods:
+
+//isDisplayed() → checks if the element is visible
+
+//isEnabled() → checks if the element is enabled
+
+//isSelected() → checks if the element (like radio/checkbox) is selected
+
+
+
